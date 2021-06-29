e5c194536e5dcd097ff9c5aa66a2085905383d43
Sofia Luna <sofialunazx@gmail.com>
2021-06-29 15:14:29 -0600

    optimizacion del codigo del componente plan

--- FrontEnd/fishingStudy-App/src/app/landing-page/components/section-plans/section-plans.component.ts
+++ FrontEnd/fishingStudy-App/src/app/landing-page/components/section-plans/section-plans.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PlansService } from '../../../services/plans.service';
 
 @Component({
   selector: 'app-section-plans',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SectionPlansComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor( private plansService:PlansService) { 
   }
 
+  ngOnInit(): void {
+   
+  }
+  planes = this.plansService.getPlans();
 }
