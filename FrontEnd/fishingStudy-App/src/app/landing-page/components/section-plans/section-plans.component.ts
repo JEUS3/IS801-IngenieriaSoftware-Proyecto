41c2d8c7e36ab27e5a4b5ac0ce3dff8f2b3f59b9
Sofia Luna <sofialunazx@gmail.com>
2021-06-30 02:23:30 -0600

    incorporacion de los botones del sidebar, optimizacion de codigo componente header y functions ,agregado de estilos al componente 404

--- FrontEnd/fishingStudy-App/src/app/landing-page/components/section-plans/section-plans.component.ts
+++ FrontEnd/fishingStudy-App/src/app/landing-page/components/section-plans/section-plans.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { PlansService } from '../../../services/plans.service';
+import { plan } from '../../../interfaces/plan.model';
 
 @Component({
   selector: 'app-section-plans',
@@ -7,12 +7,51 @@ import { PlansService } from '../../../services/plans.service';
   styleUrls: ['./section-plans.component.css']
 })
 export class SectionPlansComponent implements OnInit {
-
-  constructor( private plansService:PlansService) { 
+  
+  plans : plan[] = [
+    {
+      typePlan : 'Plan Basico',
+      title :'Lo que necesitas',
+      period : 'Mensual',
+      cost : 100,
+      description : 'Proporciona las herramientas necesarias para iniciar a impulsar su negocio permite a un asistente social seguir información estándar sobre una actividad ,miembros',
+      features : [
+        'Modulo estadistico',
+        'Modulo de mapeo',
+        ' Modulo gestion de informacion',
+        'Usuarios limitados'
+      ]
+    },
+    {
+      typePlan : 'Plan Premium',
+      title :'Lo que necesitas',
+      period : 'Mensual',
+      cost : 150,
+      description : 'El Proporciona atención complementaria para ti y tu organizacion con acceso a beneficios especiales y coberturas adicionales a las ofrecidas por el Plan basico',
+      features : [
+        'Modulo estadistico',
+        'Modulo de mapeo',
+        ' Modulo gestion de informacion',
+        'Usuarios ilimitados'
+      ]
+    },{
+      typePlan : 'Plan Personalizado',
+      title :'Lo que necesitas',
+      period : 'Mensual',
+      cost : 200,
+      description : 'Proporciona un marco flexible en el que una organización puede generar elementos de plan personalizados que encajen con las necesidades específicas.',
+      features : [
+        'Modulo estadistico',
+        'Modulo de mapeo',
+        'Modulo gestion de informacion',
+        'Usuarios ilimitados',
+      ]
+    }
+  ]
+  constructor() { 
   }
 
   ngOnInit(): void {
-   
   }
-  planes = this.plansService.getPlans();
+ 
 }
