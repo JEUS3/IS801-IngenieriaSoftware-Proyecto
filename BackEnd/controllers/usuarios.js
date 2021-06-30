0829d78ec3a272d2e69e40c4eca3a8043b85b595
HectorVL <hector_vasquez@lumminatech.com>
2021-06-30 03:49:48 -0600

    Crear la direccion del back renew

--- BackEnd/controllers/usuarios.js
+++ BackEnd/controllers/usuarios.js
@@ -3,41 +3,41 @@ const { generarJWT } = require("../helpers/jwt");
 const bcrypt = require("bcryptjs");
 const Usuario = require("../models/usuario");
 
-const test = (req=request, res=response)=>{
+const test = (req = request, res = response) => {
     res.status(200).json({
-        ok  : true,
-        msg : "El test se realizo de manera exitosa!!"
+        ok: true,
+        msg: "El test se realizo de manera exitosa!!"
     });
-}
+};
 
-const getUser = async(req=request, res=response)=>{
-    const usuarios = await Usuario.find({},"name email role google"); //{} (especificamos el filtro), 'campos separados por espacio'
+const getUser = async(req = request, res = response) => {
+    const usuarios = await Usuario.find({}, "name email role google"); //{} (especificamos el filtro), 'campos separados por espacio'
     res.status(200).json({
-        ok  : true,
+        ok: true,
         usuarios,
-        userSolic:req.uid
+        userSolic: req.uid
     });
-}
+};
 
-const newUser = async(req=request, res=response)=>{
+const newUser = async(req = request, res = response) => {
     const { email, password } = req.body;
     try {
         // Paso 1: Verificar el email
-        const emailExist = await Usuario.findOne({email});
+        const emailExist = await Usuario.findOne({ email });
         console.log(emailExist);
-        if(emailExist){
+        if (emailExist) {
             return res.status(400).json({
-                ok:false,
-                msg:"El correo ingresado ya esta siendo utilizado."
+                ok: false,
+                msg: "El correo ingresado ya esta siendo utilizado."
             })
         }
         // Paso 2: Si pasa la validacion creamos una nueva instancia de usuario
-        const usuario = new Usuario(req.body); 
+        const usuario = new Usuario(req.body);
 
         // Paso 3: Encryptamos la contraseña, como buena practica.
         // Dejar para el segundo sprint (comentar las siguientes dos lineas)
         const salt = bcrypt.genSaltSync(); //data generada de manera aletoria.
-        usuario.password = bcrypt.hashSync( password, salt);
+        usuario.password = bcrypt.hashSync(password, salt);
 
         // Paso 5: Generar JWT (Esto con el fin de disminuir las peticiones al back en caso de que la app tenga muchos usuario)
         const token = await generarJWT(usuario.id);
@@ -45,98 +45,114 @@ const newUser = async(req=request, res=response)=>{
         // Paso 4: Guardar usuario en la base de datos.
         await usuario.save();
         res.status(200).json({
-            ok  : true,
+            ok: true,
             usuario,
             token
         });
     } catch (error) {
         res.status(500).json({
-            ok:false,
-            msg:"Por favor contáctese con el administrador"
+            ok: false,
+            msg: "Por favor contáctese con el administrador"
         });
     }
 
-}
+};
 
-const updateData = async(req=request, res=response)=>{
-    const uid =  req.params.id;
-    const {name, email, role} = req.body;
+const updateData = async(req = request, res = response) => {
+    const uid = req.params.id;
+    const { name, email, role } = req.body;
     try {
         //Verificar que el usuario este registrado
         const userRegister = await Usuario.findById(uid);
-        if(!userRegister){
+        if (!userRegister) {
             return res.status(404).json({
-                ok:false,
-                msg:"El id del usurio no se encuentra en la BD."
+                ok: false,
+                msg: "El id del usurio no se encuentra en la BD."
             });
         }
 
         //Verificar la existencia del correo
-        if( email === userRegister.email){
+        if (email === userRegister.email) {
             //Actualizar los campos
-            const userUpdate = await Usuario.findByIdAndUpdate(uid, {name, role}, { new:true } );
+            const userUpdate = await Usuario.findByIdAndUpdate(uid, { name, role }, { new: true });
             return res.status(200).json({
-                ok:true,
+                ok: true,
                 userUpdate
             });
-        }else{
-            const emailExist = await Usuario.findOne({email}); //retorna null si no existe, de lo contrario la considencia.
-            if(!emailExist){
+        } else {
+            const emailExist = await Usuario.findOne({ email }); //retorna null si no existe, de lo contrario la considencia.
+            if (!emailExist) {
                 return res.status(200).json({
-                    ok:true,
+                    ok: true,
                     emailExist
                 });
             }
             return res.status(404).json({
-                ok:true,
-                msg:"El correo ya es utilizado por otro usuario."
+                ok: true,
+                msg: "El correo ya es utilizado por otro usuario."
             });
         }
 
     } catch (error) {
-        console.log("CATCH",error);
+        console.log("CATCH", error);
         res.status(500).json({
-            ok:false,
-            msg:"Por favor contáctese con el administrador"
+            ok: false,
+            msg: "Por favor contáctese con el administrador"
         });
     }
-}
+};
 
-const deleteUser = async(req=request, res=response)=>{
+const deleteUser = async(req = request, res = response) => {
     const uid = req.params.id;
     try {
         const userExist = await Usuario.findById(uid);
-        if(!userExist){
+        if (!userExist) {
             return res.status(200).json({
-                ok:false,
-                msg:"El id del usuario no existe."
+                ok: false,
+                msg: "El id del usuario no existe."
             });
         }
         await Usuario.findByIdAndDelete(uid);
         res.status(200).json({
-            ok:true,
-            msg:"El usuario se ha eliminado correctamente."
+            ok: true,
+            msg: "El usuario se ha eliminado correctamente."
         });
 
     } catch (error) {
         console.log(error);
         response.status(500).json({
-            ok:false,
-            msg:"Por favor contáctese con el administrador"
+            ok: false,
+            msg: "Por favor contáctese con el administrador"
         });
     }
-}
+};
+
+const revalidarToken = async(req = request, res = response) => {
+
+    //obtener uid
+    const { uid } = req;
+
+    //renovar nuevo JWT
+    const token = await generarJWT(uid);
+
+    return res.status(200).json({
+        ok: true,
+        uid,
+        token
+    });
+};
 
 module.exports = {
     test,
     getUser,
-    newUser:newUser,
+    newUser: newUser,
     updateData,
-    deleteUser
+    deleteUser,
+    revalidarToken
 };
 
 // Nota:
 // Advertencia de obsolescencia: Mongoose: `findOneAndUpdate ()` y `findOneAndDelete ()` sin la opción `useFindAndModify` 
 // establecida en false están en desuso. Ver: https://mongoosejs.com/docs/deprecations.html#findandmodify
 // (Use `node --trace-deprecation ...` para mostrar dónde se creó la advertencia)
-// collection.findAndModify está en desuso. Utilice findOneAndUpdate, findOneAndReplace o findOneAndDelete en su lugar. 
+// collection.findAndModify está en desuso. Utilice findOneAndUpdate, findOneAndReplace o findOneAndDelete en su lugar.
--- BackEnd/routes/usuarios.js
+++ BackEnd/routes/usuarios.js
@@ -5,32 +5,32 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
 
-const { test, getUser, newUser, updateData, deleteUser } = require("../controllers/usuarios");
+const { test, getUser, newUser, updateData, deleteUser, revalidarToken } = require("../controllers/usuarios");
 const { validarCampos } = require("../middlewares/validar-campos");
 const { validarJWT } = require("../middlewares/validar-jwt");
 const router = Router();
 
 router.get("/test", test);
 router.get("/", validarJWT, getUser);
-router.post("/",
-            [
-                check("name", "El nombre es obligatorio.").not().isEmpty(),
-                check("password", "La contrasaña es obligatorio.").not().isEmpty(),
-                check("email", "El email no es valido.").isEmail(),
-                check("role", "El rol es obligatorio.").not().isEmpty(),
-                validarCampos
-            ],
-            newUser);
-router.put("/:id",
-            [
-                validarJWT,
-                check("name", "El nombre es obligatorio.").not().isEmpty(),
-                check("email", "El email no es valido.").isEmail(),
-                check("role", "El role es obligatorio.").not().isEmpty(),
-                validarCampos
-            ],
-            updateData);
+router.get("/renew", validarJWT, revalidarToken);
+router.post("/", [
+        check("name", "El nombre es obligatorio.").not().isEmpty(),
+        check("password", "La contrasaña es obligatorio.").not().isEmpty(),
+        check("email", "El email no es valido.").isEmail(),
+        check("role", "El rol es obligatorio.").not().isEmpty(),
+        validarCampos
+    ],
+    newUser);
+router.put("/:id", [
+        validarJWT,
+        check("name", "El nombre es obligatorio.").not().isEmpty(),
+        check("email", "El email no es valido.").isEmail(),
+        check("role", "El role es obligatorio.").not().isEmpty(),
+        validarCampos
+    ],
+    updateData);
 
 router.delete("/:id", validarJWT, deleteUser);
+
 
 module.exports = router;
