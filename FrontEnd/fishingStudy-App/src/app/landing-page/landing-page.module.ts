2b8b9dcea98c4bea23348a677d877dafbd6b371f
JeanlucBoquin <jean.boquin.s@gmail.com>
2021-07-08 18:32:52 -0600

    Implementacion de la libreria ngx-scrollbar.

--- FrontEnd/fishingStudy-App/src/app/landing-page/landing-page.module.ts
+++ FrontEnd/fishingStudy-App/src/app/landing-page/landing-page.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {NgsRevealModule} from 'ngx-scrollreveal';
+import { NgsRevealModule } from 'ngx-scrollreveal';
+import { NgScrollbarModule } from 'ngx-scrollbar';
+import { PrimengModule } from '../primeng/primeng.module';
+import { MaterialModule } from '../material/material/material.module';
 
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { LandingComponent } from './landing/landing.component';
@@ -9,11 +12,9 @@ import { HeaderComponent } from './components/header/header.component';
 import { SectionAboutComponent } from './components/section-about/section-about.component';
 import { SectionFunctionComponent } from './components/section-function/section-function.component';
 import { SharedModule } from '../shared/shared.module';
-import { MaterialModule } from '../material/material/material.module';
 import { SectionPlansComponent } from './components/section-plans/section-plans.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SectionContactUsComponent } from './components/section-contact-us/section-contact-us.component';
-import { PrimengModule } from '../primeng/primeng.module';
 
 
 @NgModule({
@@ -35,7 +36,8 @@ import { PrimengModule } from '../primeng/primeng.module';
     SharedModule,
     MaterialModule,
     PrimengModule,
-    NgsRevealModule
+    NgsRevealModule,
+    NgScrollbarModule
   ]
 })
 export class LandingPageModule { }
